d3eb2c2838349eddb38a71272c3628fb021f288b
eksaarifa <81791231+eksaarifa@users.noreply.github.com>
2021-09-13 20:30:12 +0700

    Add files via upload

---
+++ quest.js
@@ -0,0 +1,20 @@
+let quest = [
+  
+  {
+    "user" : "!help",
+    "bot" : "Anda bisa mengetikkan<br>!rumah<br>!pembuat<br>!website"
+  },
+  {
+    "user" : "!rumah",
+    "bot" : "aku tinggal di indonesia"
+  },
+  {
+    "user" : "!pembuat",
+    "bot" : "Aku dibuat oleh EksaDev"
+  },
+  {
+    "user" : "!website",
+    "bot" : "https://eksaarifa.github.io/eksadevweb/"
+  }
+  
+  ]
---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('.kotak .padding .input form input');
+const send = document.querySelector('.kotak .padding .input form button');
+const form = document.querySelector('.kotak .padding .input form');
+const chatbox = document.querySelector('.kotak .padding .chatbox');
+
+let p = '<div class="incoming">';
+let l = '</div>';
+
+form.onsubmit = (e)=>{
+  e.preventDefault();
+}
+
+send.onclick = ()=>{
+  if (input != "") {
+    chatbox.innerHTML += '<div class="pembatas"><div class="outgoing">'+input.value+'</div></div>';
+    
+  
+  for (let i = 0; i < quest.length; i++) {
+    if (input.value == quest[i].user) {
+      chatbox.innerHTML += p+quest[i].bot+l;
+    }
+  }
+    
+    
+    
+    
+    
+    input.value = "";
+  }
+}
